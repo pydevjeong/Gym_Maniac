81b4f68cf484daeaabe760e46c5fca087f435496
Jeong Hun Nam <njh3421@gmail.com>
2022-09-20 19:21:22 +0900

    Update RoutePages.js

--- src/Router/RoutePages.js
+++ src/Router/RoutePages.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
 
 import Main from "../component/Main";
@@ -9,10 +10,10 @@ function RoutePages(){
   return(
     <Router>
       <Routes>
-        <Route exact path="/" element={<Main/>}/>
-        <Route exact path="mainQuiz" element={<MainQuiz/>}/>
-        <Route exact path="result" element={<ResultPage/>}/>
-        
+        <Route exact path="/*" element={<Main/>} />
+        <Route exact path="/mainQuiz" element={<MainQuiz/>}/>
+        <Route exact path="/result" element={<ResultPage/>}/>
+
       </Routes>
     </Router>
   )
