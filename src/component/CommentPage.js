6011ae28ac0ce21584293520f64fe81e3700275e
Jeong Hun Nam <njh3421@gmail.com>
2022-10-26 22:34:10 +0900

    Add commnetPage

--- src/component/CommentPage.js
+++ src/component/CommentPage.js
@@ -1,73 +1,91 @@
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import styles from './CommentPage.module.css'
+import styles from "./CommentPage.module.css";
 import { useLocation } from "react-router-dom";
+let id=0
+
+function ComponentPage() {
+  const nickName = useLocation().state.nickName;
+  const [comment, setComment] = useState("");
+  const [commentArr, setCommentArr] = useState([
+    { id: 0 ,nickName: "임시유저", value: "임시 댓글 입니다" },
+  ]);
+  const [checkSpace, setCheckSpace] = useState(false);
 
 
-function ComponentPage(props){
-  const nickName=useLocation().state.nickName
-  const [comment,setComment]=useState('')
-  const [commentArr,setCommentArr]=useState([
-    {id:'임시유저',value:'임시 댓글 입니다'}
-  ])
-  const [checkSpace,setCheckSpace]=useState(false)
-
-  const comInput=(e)=>{
-    console.log(e)
-    if(e.target.value.length!==0){
-      setCheckSpace(true)
+  const comInput = (e) => {
+    console.log(e.target.value.length);
+    if (e.target.value.length !== 0) {
+      setCheckSpace(true);
+      setComment(e.target.value);
     }
-    console.log('입력값');
-    setCheckSpace(false)
-    setComment(e.target.value)
-  }
+  };
 
-  //onKeyPress={comArrInput}
-  // const comArrInput=(e)=>{
-  //   if(e.key==='Enter'){
-  //     e.preventDefault();
-  //     const newArr=[...commentArr]
-  //     if(checkSpace){
-  //       newArr.push({id:nickName,value:comment})
-  //     }
-  //     setCommentArr(newArr)
-  //     e.target.value=''
-  //   }
-  // }
+  const comArrInput = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      const newArr = [...commentArr];
+      if (checkSpace) {
+        newArr.push({id:++id, nickName: nickName, value: comment });
+      }
+      setCommentArr(newArr);
+      e.target.value = "";
+    }
+  };
 
-  function enterBtn(e){
+  function enterBtn(e) {
     e.preventDefault();
-    console.log('ni')
-    // const newArr=[...commentArr]
-    // if(checkSpace){
-    //   newArr.push({id:nickName,value:comment})
-    // } 
-    // setCommentArr(newArr)
+    const newCommnetArr=[...commentArr]
+    console.log(checkSpace)
+    if(checkSpace){
+      newCommnetArr.push({id:++id,nickName:nickName,value:comment})
+    }
+    console.log(newCommnetArr)
+    setCommentArr(newCommnetArr)
   }
-  return(
+  return (
     <div>
-    <div className={styles.mainContainer}>
-      <h3>{nickName}님</h3>
-      <h2>자유롭게 코멘트를 남겨주세요</h2>
-      <div className={styles.commentContainer}> 
-        <textarea placeholder="입력후 Enter누르세요" rows={2} maxLength={40} type="text" className={styles.commentInput}  onChange={comInput}/>
-        <button className={styles.submitBtn} onClick={enterBtn}>Enter</button>
+      <div className={styles.mainContainer}>
+        <h3>{nickName}님</h3>
+        <h2>자유롭게 코멘트를 남겨주세요</h2>
+        <div className={styles.commentContainer}>
+          <textarea
+            placeholder="입력후 Enter누르세요"
+            rows={2}
+            maxLength={40}
+            type="text"
+            className={styles.commentInput}
+            onKeyPress={comArrInput}
+            onChange={comInput}
+          />
+          <button className={styles.submitBtn} onClick={enterBtn}>
+            Enter
+          </button>
+        </div>
+        <div className={styles.linkBox}>
+          <Link className={styles.comLink} to="/">
+            Home
+          </Link>
+        </div>
       </div>
-      <div className={styles.linkBox}>
-        <Link className={styles.comLink} to='/'>Home</Link>
-      </div>
-    </div>
       <div>
-        {commentArr.map(com=>(
-          <li key={com.id} style={{listStyle:"none",border:"1px solid #000",marginTop:"2  px"}}>
-            <span style={{color:"black"}}>{com.id} : </span>
-            <span style={{color:"black"}}>{com.value}</span>
+        {commentArr.map((com) => (
+          <li
+            key={com.id}
+            style={{
+              listStyle: "none",
+              border: "1px solid #000",
+              marginTop: "2  px",
+            }}
+          >
+            <span style={{ color: "black" }}>{com.nickName} : </span>
+            <span style={{ color: "black" }}>{com.value}</span>
           </li>
         ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default ComponentPage
+export default ComponentPage;
