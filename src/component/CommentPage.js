ede393401c333aefeaa1c0a99e7f83f5889fb690
Jeong Hun Nam <njh3421@gmail.com>
2022-10-26 22:17:45 +0900

    Add title, fix css

--- src/component/CommentPage.js
+++ src/component/CommentPage.js
@@ -3,7 +3,8 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from './CommentPage.module.css'
 import { useLocation } from "react-router-dom";
-// More functional react code
+
+
 function ComponentPage(props){
   const nickName=useLocation().state.nickName
   const [comment,setComment]=useState('')
@@ -13,30 +14,45 @@ function ComponentPage(props){
   const [checkSpace,setCheckSpace]=useState(false)
 
   const comInput=(e)=>{
+    console.log(e)
     if(e.target.value.length!==0){
       setCheckSpace(true)
     }
+    console.log('입력값');
+    setCheckSpace(false)
     setComment(e.target.value)
   }
-  const comArrInput=(e)=>{
-    if(e.key==='Enter'){
-      e.preventDefault();
-      const newArr=[...commentArr]
-      if(checkSpace){
-        newArr.push({id:nickName,value:comment})
-      }
-      setCommentArr(newArr)
-      e.target.value=''
-    }
+
+  //onKeyPress={comArrInput}
+  // const comArrInput=(e)=>{
+  //   if(e.key==='Enter'){
+  //     e.preventDefault();
+  //     const newArr=[...commentArr]
+  //     if(checkSpace){
+  //       newArr.push({id:nickName,value:comment})
+  //     }
+  //     setCommentArr(newArr)
+  //     e.target.value=''
+  //   }
+  // }
+
+  function enterBtn(e){
+    e.preventDefault();
+    console.log('ni')
+    // const newArr=[...commentArr]
+    // if(checkSpace){
+    //   newArr.push({id:nickName,value:comment})
+    // } 
+    // setCommentArr(newArr)
   }
   return(
     <div>
     <div className={styles.mainContainer}>
       <h3>{nickName}님</h3>
       <h2>자유롭게 코멘트를 남겨주세요</h2>
-      <div className={styles.commentContainer}>
-        <textarea placeholder="입력후 Enter누르세요" rows={2} maxLength={40} type="text" className={styles.commentInput}  onKeyUp={comInput} onKeyPress={comArrInput}/>
-        <button className={styles.submitBtn}>Enter</button>
+      <div className={styles.commentContainer}> 
+        <textarea placeholder="입력후 Enter누르세요" rows={2} maxLength={40} type="text" className={styles.commentInput}  onChange={comInput}/>
+        <button className={styles.submitBtn} onClick={enterBtn}>Enter</button>
       </div>
       <div className={styles.linkBox}>
         <Link className={styles.comLink} to='/'>Home</Link>
